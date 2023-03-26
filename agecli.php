290c2b30c752e0b25531102c01e8acdb5012494d
Mark Anthony Soberano <soberano.markanthony@auf.edu.ph>
2023-03-26 18:09:17 +0800

    First Commit

---
+++ agecli.php
@@ -0,0 +1,19 @@
+<?php
+
+$name = $argv[1];
+$birthYear = $argv[2];
+
+// Compute age
+$age = date('Y') - $birthYear;
+
+// Check if already a voter
+if ($age >= 18) {
+    echo "You are a voter\n";
+}elseif ($age >= 60) {
+    echo "You are a senior citizen\n";
+// Check if already a senior citizen
+}else {
+	echo "You're not a voter\n";
+}
+echo "Your name is $name, and your age is $age years old\n";
+?>	
---
+++ length.php
@@ -0,0 +1,8 @@
+<?php
+function compute($value) {
+    return ($value);
+
+}
+echo strlen("twister")*2;
+
+?>
